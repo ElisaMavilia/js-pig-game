63921eed39f796617779ae6dd25abc6051f6eeb8
ElisaMavilia <elisamavilia1@gmail.com>
2024-11-25 10:54:52 +0100

    added activePlayer

--- starter/script.js
+++ starter/script.js
@@ -17,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0]; // this array stores the scores for both players
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function(){
@@ -31,11 +33,14 @@ btnRoll.addEventListener('click', function(){
     if (dice !== 1) {
         // Add dice to current score
         currentScore += dice;
-        current0El.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore; 
     }else {
-        currentScore = 0;
         // Switch to the next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0; 
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
     }
+        
 })
 
 
