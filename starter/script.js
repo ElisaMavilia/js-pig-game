316f5dc289b3aef96774488ee9ba52e5b03da74e
ElisaMavilia <elisamavilia1@gmail.com>
2024-11-25 11:31:33 +0100

    added the active class to the active player

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,17 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
+
+
+
 
 // Selecting buttons
 const btnNew = document.querySelector('.btn--new');
@@ -33,14 +39,16 @@ btnRoll.addEventListener('click', function(){
     if (dice !== 1) {
         // Add dice to current score
         currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore; 
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
     }else {
         // Switch to the next player
         document.getElementById(`current--${activePlayer}`).textContent = 0; 
         currentScore = 0;
         activePlayer = activePlayer === 0 ? 1 : 0;
+       player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active'); 
     }
-        
 })
 
 
